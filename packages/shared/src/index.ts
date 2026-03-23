4ed4d4aeb375f3c7caf61d09fe558942d5306189
greg2012201 <greg2012201@gmail.com>
2026-03-23 22:07:33 +0100

    Update imports/exports to be compatibile with module resolution config.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,11 +2,11 @@ export type {
   CampaignStatus,
   Campaign,
   CampaignAsset,
-} from "./types/campaign";
+} from "./types/campaign.js";
 
-export type { DeviceStatus, Device } from "./types/device";
+export type { DeviceStatus, Device } from "./types/device.js";
 
-export type { Manifest, ManifestAsset } from "./types/manifest";
+export type { Manifest, ManifestAsset } from "./types/manifest.js";
 
 export type {
   EventType,
@@ -14,7 +14,7 @@ export type {
   DisplayStart,
   DisplayComplete,
   ErrorEvent,
-} from "./types/events";
+} from "./types/events.js";
 
-export { TOPICS } from "./constants/topics";
-export { EVENT_TYPES } from "./constants/event-types";
+export { TOPICS } from "./constants/topics.js";
+export { EVENT_TYPES } from "./constants/event-types.js";
