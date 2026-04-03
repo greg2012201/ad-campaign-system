7be4facf21e38533e40dc904da9c19a65b548113
greg2012201 <greg2012201@gmail.com>
2026-04-03 22:47:15 +0200

    Update migration creation config.

--- apps/backend/src/database/data-source.ts
+++ apps/backend/src/database/data-source.ts
@@ -12,6 +12,7 @@ const AppDataSource = new DataSource({
   entities: [path.join(__dirname, "/../**/*.entity.{ts,js}")],
   migrations: [path.join(__dirname, "/migrations/*.{ts,js}")],
   synchronize: false,
+  migrationsTransactionMode: "each",
   logging: true,
 });
 
